abae041720b1db3b5077206682b7fc8e38401532
x2isystems <arnold.angel@gmail.com>
2020-04-15 18:57:50 -0400

    Update update_table.sql

--- update_table.sql
+++ update_table.sql
@@ -1,16 +1,1 @@
-SET SERVEROUTPUT ON SIZE 1000000
-DECLARE
-  v_col_exists NUMBER 
-BEGIN
-  SELECT count(*) INTO v_col_exists
-    FROM user_tab_cols
-    WHERE column_name = 'EFFECTIVE_DATE'
-      AND table_name = 'DEPARTMENTS';
-
-   IF (v_col_exists = 0) THEN
-      EXECUTE IMMEDIATE 'ALTER TABLE departments ADD effective_date DATE';
-   ELSE
-    DBMS_OUTPUT.PUT_LINE('The column effective_date already exists');
-  END IF;
-END;
-/
+UPDATE TABLE DEPARTMENTS ADD COLUMN MARKET VARCHAR2(30);
